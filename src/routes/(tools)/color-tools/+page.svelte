d72dde2b81be97e2d5c99a30985cc8020ca5505f
PragnaThanam30 <pragna0830@gmail.com>
2024-07-12 23:34:53 +0530

    color-details(pragna)

---
+++ src/routes/(tools)/color-tools/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import ColorTool from './Color Tool.svelte';
+	import color details from './color details.svelte'
+</script>
+  <main>
+	<h1>Color Tool</h1>
+	<ColorTool />
+  </main>
+  <style>
+main {
+	  text-align: center;
+	  padding: 20px;
+	}
+  </style>
+  
